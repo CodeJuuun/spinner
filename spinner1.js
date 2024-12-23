fe8898535467d4b8c63fc05922ee6aa20fd1ca8e
CodeJuuun <Chomseng.a@gmail.com>
2024-12-22 23:22:47 -0500

    commented out further rotations

--- spinner1.js
+++ spinner1.js
@@ -20,19 +20,19 @@ setTimeout(() => {
   process.stdout.write("\r|   ");
 }, 900);
 
-setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 1100);
+// setTimeout(() => {
+//   process.stdout.write("\r/   ");
+// }, 1100);
 
-setTimeout(() => {
-  process.stdout.write("\r-   ");
-}, 1300);
+// setTimeout(() => {
+//   process.stdout.write("\r-   ");
+// }, 1300);
 
-setTimeout(() => {
-  process.stdout.write("\r\\  ");
-}, 1500);
+// setTimeout(() => {
+//   process.stdout.write("\r\\  ");
+// }, 1500);
 
-setTimeout(() => {
-  process.stdout.write("\r|   ");
-}, 1700);
+// setTimeout(() => {
+//   process.stdout.write("\r|   ");
+// }, 1700);
 
